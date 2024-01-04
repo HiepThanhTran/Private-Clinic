57c91c0bff97364b32c81be423b7d7c4a85a3d32
SongHau <songhau2772003@gmail.com>
2024-01-05 00:21:18 +0700

    Finish About Page

--- private_clinic/urls.py
+++ private_clinic/urls.py
@@ -3,6 +3,7 @@ from private_clinic.app import app
 
 app.add_url_rule('/', 'index', views.index)
 app.add_url_rule('/about', 'about', views.about)
+app.add_url_rule('/healthcare-staff', 'healthcare_staff', views.healthcare_staff)
 app.add_url_rule('/auth', 'auth', views.auth)
 app.add_url_rule('/appointment', 'appointment', views.appointment)
 
--- private_clinic/views.py
+++ private_clinic/views.py
@@ -10,6 +10,9 @@ def about():
     return render_template(template_name_or_list='about.html')
 
 
+def healthcare_staff():
+    return render_template(template_name_or_list='healthcare_staff.html')
+
 def auth():
     return render_template(template_name_or_list='auth.html')
 
